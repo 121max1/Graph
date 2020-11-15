4024adc18d4581553e24f56060201f499ee3862d
Elena <Elena@ELENA-PC>
2020-11-15 18:30:35 +0400

    с

--- GraphVisual/MainWindow.xaml.cs
+++ GraphVisual/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GraphVisual.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,19 +21,67 @@ namespace GraphVisual
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _addVertexuttonIsPressed = false;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
+       
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
 
         }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+        private void RenderVerter(VertexView vertex, Color color)
+        {
+            Ellipse ellipse = new Ellipse()
+            {
+                Width = VertexView.Radius,
+                Height = VertexView.Radius,
+                Stroke = new SolidColorBrush(color),
+                StrokeThickness = 14
+            };
+            GraphCanvas.Children.Add(ellipse);
+            ellipse.SetValue(Canvas.LeftProperty, vertex.X - VertexView.Radius);
+            ellipse.SetValue(Canvas.RightProperty, vertex.Y - VertexView.Radius);
+
+            TextBlock text = new TextBlock();
+            text.Text = vertex.Name;
+            text.FontSize = 18;
+            text.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            Canvas.SetLeft(text, vertex.X - 10);
+            Canvas.SetTop(text, vertex.Y + 10);
+            GraphCanvas.Children.Add(text);
+        }
+
+        private void AddVertexButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_addVertexuttonIsPressed)
+            {
+                _addVertexuttonIsPressed = true;
+                AddEdgeButton.IsEnabled = false;
+                DeleteButton.IsEnabled = false;
+                SelectVertexButton.IsEnabled = false;
+            }
+            else
+            {
+                _addVertexuttonIsPressed = false;
+                AddEdgeButton.IsEnabled = true;
+                DeleteButton.IsEnabled = true;
+                SelectVertexButton.IsEnabled = true;
+            }    
+        }
     }
 }
+
---
+++ GraphVisual/Models/VertexView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphVisual.Models
+{
+    class VertexView
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public string Name { get; set; }
+
+        public int Number { get; set; }
+
+        public static int Radius { get; set; }
+    }
+}
